1862053922fd653f92d07546247d27c97d284451
egovaflavia <egovaflvia@gmail.com>
2020-02-18 16:50:26 +0700

    Video 05

---
+++ 03-Data-Type/app.js
@@ -0,0 +1,39 @@
+// Type data primitif, di simpan dalam stack memory
+
+// string
+const name = 'John Doe';
+// console.log(typeof name);
+
+// menentukan type data dari suatu variable typeof
+
+// number
+const age = 33;
+// console.log(typeof age);
+
+
+// Boolean
+const hasKid = false;
+// console.log(typeof hasKid);
+
+
+// Null
+const car = null;
+console.log(typeof car);
+
+
+// Undifined
+let test;
+
+const sym = Symbol();
+
+// Type data referensi
+// Array
+const hobbies = ['movies', 'music'];
+// console.log(hobbies[0]);
+
+const address = {
+    city: 'Padang',
+    zip_code: '32132'
+}
+// console.log(address.city);
+// console.log(typeof address.zip_code);
