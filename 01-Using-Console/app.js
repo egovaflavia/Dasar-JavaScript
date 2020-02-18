26a3c08aac028d8e5c4a08e7fadb2453307229ea
egovaflavia <egovaflvia@gmail.com>
2020-02-18 16:24:55 +0700

    first commit

---
+++ 01-Using-Console/app.js
@@ -0,0 +1,21 @@
+console.log('Hello, World');
+// alert('Hello World')
+console.log(123)
+console.log(true);
+var greeting = 'Hello World'
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+});
+console.table({
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+});
+
+console.error('This some error');
---
+++ 02-Variable/app.js
@@ -0,0 +1,64 @@
+// // var, let, conts
+
+// var nama = 'John Doe';
+// console.log(nama);
+
+// nama = 'Stave Smith';
+// console.log(nama);
+
+// // initialitation
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello, From JavaScript';
+// console.log(greeting);
+
+// // Aturan penulisan variabel javascript
+
+// var variable;
+// var _variable;
+// var vari_able;
+// var $variable;
+// var vari$able;
+// var variable123;
+// var vari123able123;
+
+// // multiwords
+// var firstname;
+// var firstName; // Cemal Case
+// var FirstName; // Pascal
+// var first_name;
+
+// /**
+//  * Multi line komentar
+//  * 
+//  * 
+//  */
+
+// // let
+// let name = 'Jonh Doe';
+// console.log(name);
+
+// name = 'Sara';
+// console.log(name);
+
+// conts
+// const name = 'John Doe';
+// console.log(name);
+
+// name = 'Sara'; // Akan Error;
+// // Jika Conts berupa array / object, bisa merubah isi properti ny
+
+// const person = {
+//     name: 'John',
+//     age: 30
+// }
+// console.log(person);
+
+// person.name = 'Sara';
+// person.age = 23;
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4];
+numbers.push('a', 6); // push memasukan data dlm deret aray
+console.log(numbers);
