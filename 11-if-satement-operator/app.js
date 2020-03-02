c554d64d18b24ab2fa27f53191c35d6e7400466c
egovaflavia <egovaflvia@gmail.com>
2020-03-02 15:54:57 +0700

    Ok

---
+++ 11-if-satement-operator/app.js
@@ -0,0 +1,27 @@
+const id = 100;
+
+// == value sama
+// if (id == 100) {
+//     console.log('CORRECT');
+
+// } else {
+//     console.log('INCORRECT');
+
+// }
+
+// if (id === 100) {
+//     console.log('CORRECT');
+
+// } else {
+//     console.log('INCORRECT');
+
+// }
+
+// Mencari nialai suatu variable
+if (id) {
+    console.log(`ID value is ${id}`);
+
+} else {
+    console.log('NULL');
+
+}
